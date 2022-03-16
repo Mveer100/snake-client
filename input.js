49e33a0ce3898ac69b1f9b3d267dba0abf9d8268
Maxwell Veer <maxaveer@gmail.com>
2022-03-16 22:08:10 +0000

    added a folder for constants and now we can move around and send messages on the screen when i want to

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -11,6 +14,18 @@ const handleUserInput = function (key) {
   process.stdout.write('.');
   if (key === '\u0003') {
     process.exit();
+  } else if (key === "w") {
+    connection.write("Move: up")
+    connection.write("Say: Movin down")
+  } else if (key === "a") {
+    connection.write("Move: left")
+    connection.write("Say: Movin right")
+  } else if (key === "s") {
+    connection.write("Move: down")
+    connection.write("Say: Movin up")
+  } else if (key === "d") {
+    connection.write("Move: right")
+    connection.write("Say: Movin left")
   }
 };
 
